3ab8e7ca0d99ab72df8f64c11a353a98d4be754d
Maja Konopka <maja.konopka.5@gmail.com>
2024-06-02 15:31:57 +0200

    utworzenie strony dodawania

--- ContactsBook.py
+++ ContactsBook.py
@@ -23,6 +23,44 @@ class ContactsBook:
         self.manageContactPage = Frame(self.rootMain)
         self.manageContactPage.pack()
 
+        add_button = tk.Button(self.homePage, text="Add Contact", command=self.addContact)
+        add_button.pack(pady=10)
 
         # Uruchomienie okna
         self.rootMain.mainloop()
+
+    def addContact(self):
+        def submit():
+            print("submitted")
+
+        def toTheHomePage():
+            # Powrót do strony głównej
+            self.homePage.pack()
+            self.addContactPage.pack_forget()
+
+        # Chowanie poprzedniej strony
+        self.homePage.pack_forget()
+        self.addContactPage.pack()
+
+        # Utworzenie inputów
+        label1 = tk.Label(self.addContactPage, text="Name:")
+        label1.grid(row=0, column=0, padx=5, pady=5)
+        entry1 = tk.Entry(self.addContactPage)
+        entry1.grid(row=0, column=1, padx=5, pady=5)
+
+        label2 = tk.Label(self.addContactPage, text="Surname:")
+        label2.grid(row=1, column=0, padx=5, pady=5)
+        entry2 = tk.Entry(self.addContactPage)
+        entry2.grid(row=1, column=1, padx=5, pady=5)
+
+        label3 = tk.Label(self.addContactPage, text="Phone Number:")
+        label3.grid(row=2, column=0, padx=5, pady=5)
+        entry3 = tk.Entry(self.addContactPage)
+        entry3.grid(row=2, column=1, padx=5, pady=5)
+
+        # Przyciski
+        submit_button = tk.Button(self.addContactPage, text="AddContact", command=submit)
+        submit_button.grid(row=3, column=0, padx=5, pady=5)
+
+        clear_button = tk.Button(self.addContactPage, text="Return", command=toTheHomePage)
+        clear_button.grid(row=3, column=1, padx=5, pady=5)
