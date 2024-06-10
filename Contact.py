0f4ee0bc590d3bfa10a5a5175a5f1450bf48489e
Maja Konopka <maja.konopka.5@gmail.com>
2024-06-10 23:18:50 +0200

    dokumentacja do contact

--- Contact.py
+++ Contact.py
@@ -1,6 +1,15 @@
 import uuid
 class Contact:
     def __init__(self,name,surname,phoneNumber):
+        """
+            Inicjalizuje nową instancję klasy Contact z podanym imieniem, nazwiskiem i numerem telefonu.
+            Automatycznie generowany jest unikalny identyfikator UUID.
+
+            Parametry:
+            name (str): Imię kontaktu.
+            surname (str): Nazwisko kontaktu.
+            phoneNumber (str): Numer telefonu kontaktu.
+        """
         self.name = name
         self.surname = surname
         self.phoneNumber = phoneNumber
